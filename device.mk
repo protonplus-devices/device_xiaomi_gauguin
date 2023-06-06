047c72973713976b342c12bed1429206320393b5
prajwal-walia <prajwalahluwalia5@gmail.com>
2023-06-06 14:19:00 +0530

    gauguin: nuke overlay-lineage

--- device.mk
+++ device.mk
@@ -22,8 +22,7 @@ $(call inherit-product, vendor/xiaomi/gauguin/gauguin-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp 
 
 PRODUCT_PACKAGES += \
     SettingsOverlayM2007J17C \
